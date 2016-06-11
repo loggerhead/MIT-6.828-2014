065270490a1f891148e7f352732456501c3cc28a
loggerhead <lloggerhead@gmail.com>
2016-06-11 18:01:12 +0800

    finished Exercise 9 & 10

--- kern/pmap.c
+++ kern/pmap.c
@@ -587,6 +587,17 @@ int
 user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 {
 	// LAB 3: Your code here.
+	uintptr_t vanum = (uintptr_t) va;
+	uintptr_t begin = ROUNDDOWN((uintptr_t) va, PGSIZE);
+	uintptr_t end = ROUNDUP((uintptr_t) va + len, PGSIZE);
+
+	for (; begin < end; begin += PGSIZE) {
+		pte_t *ppte = pgdir_walk(env->env_pgdir, (const void *) begin, 0);
+		if ((*ppte & perm) != perm) {
+			user_mem_check_addr = (begin < vanum ? vanum : begin);
+			return -E_FAULT;
+		}
+	}
 
 	return 0;
 }
