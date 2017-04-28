42d08ab704a84be63699dace5665408e3677f0cc
loggerhead <lloggerhead@gmail.com>
2017-04-28 21:55:58 +0800

    finished exercise 4

--- kern/trap.c
+++ kern/trap.c
@@ -146,17 +146,18 @@ trap_init_percpu(void)
 
 	// Setup a TSS so that we get the right stack
 	// when we trap to the kernel.
-	ts.ts_esp0 = KSTACKTOP;
-	ts.ts_ss0 = GD_KD;
+	int i = thiscpu->cpu_id;
+	thiscpu->cpu_ts.ts_esp0 = KSTACKTOP - i * (KSTKSIZE + KSTKGAP);
+	thiscpu->cpu_ts.ts_ss0 = GD_KD;
 
 	// Initialize the TSS slot of the gdt.
-	gdt[GD_TSS0 >> 3] = SEG16(STS_T32A, (uint32_t) (&ts),
-					sizeof(struct Taskstate) - 1, 0);
-	gdt[GD_TSS0 >> 3].sd_s = 0;
+	uint16_t gdt_i = (GD_TSS0 >> 3) + i;
+	gdt[gdt_i] = SEG16(STS_T32A, (uint32_t) thiscpu, sizeof(struct Taskstate) - 1, 0);
+	gdt[gdt_i].sd_s = 0;
 
 	// Load the TSS selector (like other segment selectors, the
 	// bottom three bits are special; we leave them 0)
-	ltr(GD_TSS0);
+	ltr(GD_TSS0 + thiscpu->cpu_id * sizeof(struct Segdesc));
 
 	// Load the IDT
 	lidt(&idt_pd);
