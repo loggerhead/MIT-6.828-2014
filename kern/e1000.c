0346cf28bfff16c469130ca5aca357a534409a6a
loggerhead <lloggerhead@gmail.com>
2017-05-13 10:54:29 +0800

    finished exercise 10

--- kern/e1000.c
+++ kern/e1000.c
@@ -6,16 +6,45 @@
 
 // the location of the virtual memory mapping for the E1000's BAR 0
 volatile uint32_t *attached_e1000;
+// Allocate a region of memory for the transmit descriptor list.
+// Software should insure this memory is aligned on a paragraph (16-byte) boundary.
 struct tx_desc tx_ring[NTXDESC] __attribute__ ((aligned (16)));
-packet_buffer tx_desc_buffers[NTXDESC];
+char tx_desc_buffers[NTXDESC][TPACK_MAX_SIZE];
+// Allocate a region of memory for the receive descriptor list.
+// Software should insure this memory is aligned on a paragraph (16-byte) boundary.
+struct rx_desc rx_ring[NRXDESC] __attribute__ ((aligned (16)));
+char rx_desc_buffers[NTXDESC][RPACK_MAX_SIZE];
 
 
-#define tdtr_ptr (&attached_e1000[TDT])
+#define TDTR_PTR (&attached_e1000[TDT])
+#define RDTR_PTR (&attached_e1000[RDT])
 
 
 inline int is_td_free(int index) {
 	return (tx_ring[index].status & TDESC_STATUS_DD) == TDESC_STATUS_DD;
 }
+
+inline void set_ral0(int i, uint8_t byte) {
+	((char *) &attached_e1000[RAL(0)])[i] = byte;
+}
+
+inline void set_rah0(int i, uint8_t byte) {
+	((char *) &attached_e1000[RAH(0)])[i] = byte;
+}
+
+// simply hard-code QEMU's default MAC address of 52:54:00:12:34:56
+inline void set_mac_address() {
+	set_ral0(0, 0x52);
+	set_ral0(1, 0x54);
+	set_ral0(2, 0x00);
+	set_ral0(3, 0x12);
+
+	set_rah0(0, 0x34);
+	set_rah0(1, 0x56);
+	set_rah0(2, 0);
+	set_rah0(3, 0);
+}
+
 
 // LAB 6: Your driver code here
 
@@ -23,34 +52,32 @@ int pci_attach_82540em(struct pci_func *f) {
 	pci_func_enable(f);
 	attached_e1000 = mmio_map_region(f->reg_base[0], f->reg_size[0]);
 
-	// Allocate a region of memory for the transmit descriptor list.
-	// Software should insure this memory is aligned on a paragraph (16-byte) boundary.
-	physaddr_t td_addr = PADDR(tx_ring);
-	size_t td_len = sizeof(tx_ring);
+	// Transmit Initialization
 
 	// The Transmit Descriptor Base Address of the region
 	// NOTICE: physical address!
-	attached_e1000[TDBAL] = td_addr;
+	attached_e1000[TDBAL] = PADDR(tx_ring);
 	attached_e1000[TDBAH] = 0;
-
 	// The size (in bytes) of the descriptor ring.
 	// This value must be a multiple of 128 bytes
-	attached_e1000[TDLEN] = td_len;
-
-	// The Transmit Descriptor Head and Tail
+	attached_e1000[TDLEN] = sizeof(tx_ring);
+	// Transmit Descriptor Head and Tail
 	attached_e1000[TDH] = 0;
 	attached_e1000[TDT] = 0;
-
 	// Initialize the Transmit Control Register:
 	//
 	// * set the Enable (TCTL.EN) bit to 1b for normal operation.
 	// * set the Pad Short Packets (TCTL.PSP) bit to 1b.
 	// * configure the Collision Threshold (TCTL.CT) to the desired value (10h).
 	// * configure the Collision Distance (TCTL.COLD) to 40h.
-	attached_e1000[TCTL] = TCTL_EN(1) | TCTL_PSP(1) | TCTL_CT(0x10) | TCTL_COLD(0x40);
-
+	attached_e1000[TCTL] = TCTL_EN(1)
+		| TCTL_PSP(1)
+		| TCTL_CT(0x10)
+		| TCTL_COLD(0x40);
 	// the Transmit IPG (TIPG) register
-	attached_e1000[TIPG] = TIPG_IPGT(0xA) | TIPG_IPGR1(0x8) | TIPG_IPGR2(0xC);
+	attached_e1000[TIPG] = TIPG_IPGT(0xA)
+		| TIPG_IPGR1(0x8)
+		| TIPG_IPGR2(0xC);
 
 	// init transmit descriptor ring
 	memset(tx_ring, 0, sizeof(tx_ring));
@@ -60,6 +87,31 @@ int pci_attach_82540em(struct pci_func *f) {
 		tx_ring[i].status = TDESC_STATUS_DD;
 	}
 
+	// Receive Initialization
+
+	// setup RAL[0]/RAH[0] to store the MAC address of the Ethernet controller.
+	set_mac_address();
+	// Initialize the MTA (Multicast Table Array) to 0b
+	memset((void *) &attached_e1000[MTA_BASE], 0, MTA_SIZE);
+	// For now, don't configure the card to use interrupts
+	attached_e1000[IMS] = 0;
+	// init the receive descriptor list
+	attached_e1000[RDBAL] = PADDR(rx_ring);
+	attached_e1000[RDBAH] = 0;
+	attached_e1000[RDLEN] = sizeof(rx_ring);
+	// Receive Descriptor Head and Tail
+	// Head point to the first valid receive descriptor
+	attached_e1000[RDH] = PADDR(rx_ring);
+	// Tail point to one descriptor beyond the last valid descriptor
+	attached_e1000[RDT] = PADDR(rx_ring + NRXDESC);
+	// Receive Control (RCTL) register
+	attached_e1000[RCTL] = RCTL_EN(1)
+		| RCTL_LPE(0)
+		| RCTL_LBM(0)
+		| RCTL_BAM(1)
+		| RCTL_BSIZE(0)
+		| RCTL_SECRC(1);
+
 	return 0;
 }
 
@@ -68,11 +120,11 @@ int send_data_at(void *addr, uint16_t len) {
 		return 0;
 	}
 
-	int next = *tdtr_ptr;
+	int next = *TDTR_PTR;
 	int remain = len;
 
 	do {
-		size_t n = MIN(PACKET_MAX_SIZE, remain);
+		size_t n = MIN(TPACK_MAX_SIZE, remain);
 		remain -= n;
 
 		// checking that the next descriptor is free
@@ -88,7 +140,7 @@ int send_data_at(void *addr, uint16_t len) {
 			}
 			tx_ring[next].status ^= TDESC_STATUS_DD;
 			// updating the TDT (transmit descriptor tail) register
-			*tdtr_ptr = (next + 1) % NTXDESC;
+			*TDTR_PTR = (next + 1) % NTXDESC;
 			// if the transmit queue is full
 		} else {
 			// simply drop the packet
--- kern/e1000.h
+++ kern/e1000.h
@@ -4,36 +4,65 @@
 #include <inc/types.h>
 #include <kern/pci.h>
 
-// Transmit Descriptor
-#define TCTL   (0x00400 / 4)
-#define TDBAL  (0x03800 / 4)
-#define TDBAH  (0x03804 / 4)
-#define TDLEN  (0x03808 / 4)
-#define TDH    (0x03810 / 4)
-#define TDT    (0x03818 / 4)
-#define TIPG   (0x00410 / 4)
+#define _GEN_CONSTANTS(XX) \
+	XX(TCTL  , 0x00400)			\
+	XX(TDBAL , 0x03800)			\
+	XX(TDBAH , 0x03804)			\
+	XX(TDLEN , 0x03808)			\
+	XX(TDH   , 0x03810)			\
+	XX(TDT   , 0x03818)			\
+	XX(TIPG  , 0x00410)			\
+	XX(RCTL  , 0x00100)			\
+	XX(RDBAL , 0x02800)			\
+	XX(RDBAH , 0x02804)			\
+	XX(RDLEN , 0x02808)			\
+	XX(RDH   , 0x02810)			\
+	XX(RDT   , 0x02818)			\
+	XX(RAL_BASE, 0x05400)			\
+	XX(RAH_BASE, 0x05404)			\
+	XX(MTA_BASE, 0x05200)			\
+	XX(IMS     , 0x000D0)
+
+enum {
+#define XX(NAME, OFFSET) NAME = ((OFFSET) / 4),
+	  _GEN_CONSTANTS(XX)
+#undef XX
+};
+#undef _GEN_CONSTANTS
+
+#define RAL(i) (RAL_BASE + 8 * (i))
+#define RAH(i) (RAH_BASE + 8 * (i))
 
 // Transmit Control register
 #define TCTL_EN(x)    ((x) << 1)
 #define TCTL_PSP(x)   ((x) << 3)
 #define TCTL_CT(x)    ((x) << 4)
 #define TCTL_COLD(x)  ((x) << 12)
-
 // Transmit IPG register
 #define TIPG_IPGT(x)  (x)
 #define TIPG_IPGR1(x) ((x) << 10)
 #define TIPG_IPGR2(x) ((x) << 20)
+// Receive Control register
+#define RCTL_EN(x)    ((x) << 1)
+#define RCTL_LPE(x)   ((x) << 5)
+#define RCTL_LBM(x)   ((x) << 6)
+#define RCTL_RDMTS(x) ((x) << 8)
+#define RCTL_MO(x)    ((x) << 12)
+#define RCTL_BAM(x)   ((x) << 15)
+#define RCTL_BSIZE(x) ((x) << 16)
+#define RCTL_SECRC(x) ((x) << 26)
 
 #define TDESC_CMD_RS    (1 << 3)
 #define TDESC_CMD_EOP   1
 #define TDESC_STATUS_DD 1
 
 #define NTXDESC 32
-#define PACKET_MAX_SIZE 1528
+#define NRXDESC 256
+#define TPACK_MAX_SIZE 1528
+#define RPACK_MAX_SIZE 2048
+#define MTA_SIZE (128 * 4)
 
-typedef char packet_buffer[PACKET_MAX_SIZE];
-
-// the Legacy Transmit Descriptor
+// Legacy Transmit Descriptor format (spec section 3.3.3):
 //
 // 63            48 47   40 39   32 31   24 23   16 15             0
 // +---------------------------------------------------------------+
@@ -41,8 +70,6 @@ typedef char packet_buffer[PACKET_MAX_SIZE];
 // +---------------+-------+-------+-------+-------+---------------+
 // |    Special    |  CSS  | Status|  Cmd  |  CSO  |    Length     |
 // +---------------+-------+-------+-------+-------+---------------+
-//
-// spec 3.3.3 Legacy Transmit Descriptor Format
 struct tx_desc {
 	uint64_t addr;
 	uint16_t length;
@@ -50,6 +77,23 @@ struct tx_desc {
 	uint8_t cmd;
 	uint8_t status;
 	uint8_t css;
+	uint16_t special;
+} __attribute__((packed));
+
+// Receive Descriptor format (spec section 3.2.3):
+//
+// 63            48 47   40 39   32 31           16 15             0
+// +---------------------------------------------------------------+
+// |                         Buffer address                        |
+// +---------------+-------+-------+---------------+---------------+
+// |    Special    | Errors| Status|Packet Checksum|    Length     |
+// +---------------+-------+-------+---------------+---------------+
+struct rx_desc {
+	uint64_t addr;
+	uint16_t length;
+	uint16_t checksum;
+	uint8_t status;
+	uint8_t errors;
 	uint16_t special;
 } __attribute__((packed));
 
